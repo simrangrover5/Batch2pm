f1584fc365649c235ed48c22b73f5cc610665d88
simrangrover5 <simrangrover5@gmail.com>
2020-02-19 15:33:33 +0530

    flask code

--- flaskproject/app1.py
+++ flaskproject/app1.py
@@ -1,19 +1,44 @@
-from flask import Flask
+from flask import Flask,render_template,request
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "hello world"
+    return render_template("nav.html")
+    #return "hello world"
 
 
-@app.route("/home/")
-def home():
-    return "<h1 style='color:blue'>Welcome to my home of python</h1>"
+@app.route("/home/<var>/")
+def home(var):
+    return render_template("one.html",name=var)
+    #return "<h1 style='color:blue'>Welcome to my home of python</h1>"
 
-@app.route("/home/<var>/<int:m>")
-def name(var,m):
-    return f"<h1 style='color:red;font-style:italic'>Your subject is {var} and your marks is {m}</h1>"
+@app.route("/home/<var>/<int:m1>/<int:m2>/<int:m3>/")
+def name(var,m1,m2,m3):
+    d = {
+        'name':var,
+        'science':m1,
+        'maths':m2,
+        'english':m3
+    }
+    return render_template("one.html",data=d)
+    #return f"<h1 style='color:red;font-style:italic'>Your subject is {var} and your marks is {m}</h1>"
+
+
+@app.route('/login/')
+def login():
+    return render_template("login.html")
+
+
+@app.route("/afterlogin/",methods=['POST'])
+def afterlogin():
+    email = request.form.get('email')
+    password = request.form['pswd']
+    return f"Email {email} and password {password}"
+
+@app.route("/signup/")
+def signup():
+    return render_template("signup.html")
 
 app.run(host="localhost",port=80,debug=True)
